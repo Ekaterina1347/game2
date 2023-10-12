2539a15d324794dab28a96482c56c415362b3cb2
Ekaterina1347 <147511443+Ekaterina1347@users.noreply.github.com>
2023-10-12 18:02:10 +0500

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -13,15 +13,19 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-    def fire(self):
-        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20, -20)
-        bullets.add(bullet)
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height-80:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height-80:
+            self.rect.y += self.speed
+    
 
 back = (200,255,255)
 win_width = 700
@@ -30,15 +34,17 @@ window = display.set_mode((win_width, win_height))
 display.set_caption('Пинг-Понг')
 window.fill(back)
 
+speed_x =2
+speed_y = 2
+
 clock = time.Clock()
 FPS = 60
 run = True 
-
 finish = False
 
 sprite1 = Player('png-clipart-football-ball-sport-monochrome.png',200,200,65,40,40)
-sprite2 = Player('racket.png',30,200,15,100,150)
-sprite3 = Player('racket.png',520,200,15,100,150)
+sprite2 = Player('racket.png',30,200,15,100,10)
+sprite3 = Player('racket.png',520,200,15,100,10)
 
 
 while run:
@@ -50,13 +56,20 @@ while run:
         window.fill(back)
 
         sprite1.update()
+        sprite2.update_l()
+        sprite3.update_r()
+
+        sprite1.rect.x += speed_x
+        sprite1.rect.y += speed_y
+
+        if sprite.collide_rect(sprite2, sprite1) or sprite.collide_rect(sprite3, sprite1):
+            speed_x *=-1
+            speed_y *= 1
+        if sprite1.rect.x<0:
+            finish=True
+
         sprite1.reset()
-
-        sprite2.update()
         sprite2.reset()
-
-        sprite3.update()
         sprite3.reset()
-    
     display.update()
     clock.tick(FPS)
