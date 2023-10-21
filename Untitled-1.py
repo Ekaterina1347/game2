0ccf166df9640fe1a9414bc0b17e97414232e820
Ekaterina1347 <147511443+Ekaterina1347@users.noreply.github.com>
2023-10-21 13:32:10 +0500

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -46,6 +46,12 @@ sprite1 = Player('png-clipart-football-ball-sport-monochrome.png',200,200,65,40,
 sprite2 = Player('racket.png',30,200,15,100,10)
 sprite3 = Player('racket.png',520,200,15,100,10)
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+
 
 while run:
     for e in event.get():
@@ -65,8 +71,18 @@ while run:
         if sprite.collide_rect(sprite2, sprite1) or sprite.collide_rect(sprite3, sprite1):
             speed_x *=-1
             speed_y *= 1
+            
         if sprite1.rect.x<0:
             finish=True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        if sprite1.rect.x > win_width:
+           finish = True
+           window.blit(lose2, (200, 200))
+           game_over = True
+
+
 
         sprite1.reset()
         sprite2.reset()
